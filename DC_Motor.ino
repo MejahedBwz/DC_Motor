5daf8b333173ca0fb65c9a50757711d21ec32923
MejahedBwz <86706607+MejahedBwz@users.noreply.github.com>
2021-07-12 11:20:24 +0300

    Update DC_Motor.ino

--- DC_Motor.ino
+++ DC_Motor.ino
@@ -1,54 +1,52 @@
 
-int l1 = 3;
-int r1 = 5;
-int l2 = 6;
-int r2 = 9;
-
-
-void setup() {
+// محرك A 
+int enA = 10;
+int in1 = 3;
+int in2 = 4;
+// محرك B 
+int enB = 11;
+int in3 = 6;
+int in4 = 7;
+//الزر و المقاومة المتحركة
+int buttonPin = 8;
+int potPin = 0;
  
-  pinMode(l1,OUTPUT);
-  pinMode(r1,OUTPUT);
-  pinMode(l2,OUTPUT);
-  pinMode(l2,OUTPUT);
+void setup()
+{
+                                                   // خلي كل دبابيس التحكم في المحرك على OUTPUT
+  pinMode(enA, OUTPUT);
+  pinMode(enB, OUTPUT);
+  pinMode(in1, OUTPUT);
+  pinMode(in2, OUTPUT);
+  pinMode(in3, OUTPUT);
+  pinMode(in4, OUTPUT);
+  pinMode(buttonPin, INPUT_PULLUP);
+ 
+   
+                                                                     //  الوضع الاولي للمحركات  
+  digitalWrite(in1, LOW);
+  digitalWrite(in2, LOW);
+  digitalWrite(in3, LOW);
+  digitalWrite(in4, LOW);
+  delay(300) ;
+}
+ 
+void loop()
+{
+  int speed = analogRead(potPin) / 4;           // التحكم في السرعة  باستخدام المقاومة المتحركة 
+  boolean reverse = digitalRead(buttonPin);   // التحكم في الاتجاه عند الضغط على الزر يعكس الاتجاه
+  setMotor(speed, reverse); // استدعاء الدالة 
   
 }
-void forward()                  // تعريف دالة للاتجاة للامام 
-{
-   digitalWrite(l1,1);
-   digitalWrite(r1,0);
-   digitalWrite(l2,0);
-   digitalWrite(r2,1);
-  }
-
-  void backward()               // تعريف دالة للاتجاة للخلف
-{
-   digitalWrite(l1,0);
-   digitalWrite(r1,1);
-   digitalWrite(l2,1);
-   digitalWrite(r2,0);
-  }
-  
-
- void Stop()                        // تعريف دالة للتوقف
-{
-   digitalWrite(l1,0);
-   digitalWrite(r1,0);
-   digitalWrite(l2,0);
-   digitalWrite(r2,0);
-  }
-
-  
-void loop() {
  
-   forward();
-   delay(10000);
-   Stop();
-   delay(2000);
-   backward();
-   delay(10000);
-   Stop();
-   delay(2000);
-
-
+void setMotor(int speed, boolean reverse)              // دالة للتحكم في سرعة و اتجاه المحركات 
+{
+  analogWrite(enA, speed);
+  analogWrite(enB, speed);
+  digitalWrite(in1, ! reverse);
+  digitalWrite(in2, reverse);
+  digitalWrite(in3, ! reverse);
+  digitalWrite(in4, reverse);
+  
 }
+ 
